38913d3f8a77a389573bdecc16fd82fa7ac6e977
katehiley <69643349+katehiley@users.noreply.github.com>
2020-12-10 14:11:29 +0000

    Create videoPlayer.js

---
+++ videoPlayer.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	//if not a https connection force a https connection
+	if (location.protocol !== 'https:') {
+		location.replace(`https:${location.href.substring(location.protocol.length)}`);
+	"#lion".get(0).play()
+  }
+}
